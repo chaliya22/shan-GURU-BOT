cbbdb50b428c5f922fafa5855ffed47e6184272f
chaliya22 <154024096+chaliya22@users.noreply.github.com>
2023-12-17 06:12:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94729569571', 'me', true],
+  ['94729569571', 'sl shan', true], 
   [''] 
 ] //Number of owners
 
